f98e8ecc31f3dd0dc073ec5bbc9c7104e59a1a3a
LittleNasia <67862040+LittleNasia@users.noreply.github.com>
2024-07-09 00:36:14 +0400

    ignore warnings related to conversions for functions where it doesnt make sense

--- nina-chess/utils.h
+++ nina-chess/utils.h
@@ -74,10 +74,13 @@ forceinline size_t pext(Bitboard b, Bitboard mask)
 	return _pext_u64(b, mask);
 }
 
+#pragma warning( push )
+#pragma warning (disable:4244)
 forceinline uint32_t popcnt(Bitboard bb)
 {
 	return __popcnt64(bb);
 }
+#pragma warning( pop ) 
 
 forceinline Bitboard pop_bit(Bitboard& bb)
 {
@@ -86,10 +89,13 @@ forceinline Bitboard pop_bit(Bitboard& bb)
 	return lsb;
 }
 
+#pragma warning( push )
+#pragma warning ( disable:4244 )
 forceinline uint32_t bit_index(const Bitboard bb)
 {
 	return _tzcnt_u64(bb);
 }
+#pragma warning( pop ) 
 
 inline constexpr Bitboard row_bitmasks[board_rows] =
 {
