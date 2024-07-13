32269013b9ad408f603390966da393f08e6a6416
LittleNasia <67862040+LittleNasia@users.noreply.github.com>
2024-07-13 15:54:35 +0400

    cute things doing cute things

---
+++ test/bench_executables.py
@@ -0,0 +1,80 @@
+import numpy as np
+from scipy import stats
+import subprocess
+import glob
+import random
+import os
+
+
+MIN_RUNS = 5
+
+EXECUTABLE_REJECTED = 0
+EXECUTABLE_NOT_REJECTED_NOR_ACCEPTED = 1
+EXECUTABLE_ACCEPTED = 2
+
+def benchmarkFiles(runs, defaultFile, *filenames):
+    speeds = {}
+    statuses = {}
+
+    if runs < MIN_RUNS:
+        raise Exception(f"The number of bench runs cannot be lower than MIN_RUNS ({MIN_RUNS})")
+
+    files = [ defaultFile ]
+    for file in filenames:
+        files.append(file)
+        
+    for file in files:
+        speeds[file] = []
+        statuses[file] = EXECUTABLE_NOT_REJECTED_NOR_ACCEPTED
+
+    run = 0
+    while len(files) > 1:
+        run += 1
+        print("Run number", run)
+        random.shuffle(files)
+
+        for file in files:
+            cmd = f"\"{file}\""
+            print("\tRunning", cmd)
+            speed = int(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.splitlines()[0])
+            speeds[file].append(speed)
+            print("\t\tachieved speed", speed)
+
+
+        defaultFileScores = speeds[defaultFile]
+        for file in files:
+            if file == defaultFile:
+                continue
+
+            if run < 5:
+                continue
+
+            if len(file) <= 1:
+                break
+
+            currFileScores = speeds[file]
+
+            meanCurrent = np.mean(currFileScores)
+            meanDefault = np.mean(defaultFileScores)
+            std1 = np.std(currFileScores, ddof=1)
+            std2 = np.std(defaultFileScores, ddof=1)
+
+            t_stat, p_value = stats.ttest_ind(currFileScores, defaultFileScores, equal_var=False)
+
+            print(f"\t{file}: Mean = {meanCurrent:.2f}, Std = {std1:.2f}, N = {len(currFileScores)}")
+            print(f"\t{defaultFile}: Mean = {meanDefault:.2f}, Std = {std2:.2f}, N = {len(defaultFileScores)}")
+            print(f"\tT-statistic = {t_stat:.2f}, P-value = {p_value:.2f}")
+
+            alpha = 0.05
+            if p_value < alpha:
+                if meanCurrent > meanDefault:
+                    statuses[file] = EXECUTABLE_ACCEPTED
+                else:
+                    statuses[file] = EXECUTABLE_REJECTED
+                files.remove(file)
+            
+
+        if run >= runs:
+            break
+
+    return statuses
---
+++ test/run_benchmark.py
@@ -0,0 +1,57 @@
+import glob
+import random
+import os
+import stat
+import subprocess
+import numpy as np
+from scipy import stats
+import shutil
+import bench_executables
+
+files = glob.glob("C:\\Users\\Nasia and Ilusia\\Desktop\\test folder\\*.exe")
+PREV_VERSION_CLONE_DEST = "./test/clone"
+
+NEW_EXECUTABLE_NAME_WITHOUT_EXTENSION = "tested"
+OLD_EXECUTABLE_NAME_WITHOUT_EXTENSION = "default"
+
+NEW_EXECUTABLE_NAME = f"{NEW_EXECUTABLE_NAME_WITHOUT_EXTENSION}.exe"
+OLD_EXECUTABLE_NAME = f"{OLD_EXECUTABLE_NAME_WITHOUT_EXTENSION}.exe"
+
+NEW_EXECUTABLE_PATH = f"./test/{NEW_EXECUTABLE_NAME}"
+OLD_EXECUTABLE_PATH = f"./test/{OLD_EXECUTABLE_NAME}"
+
+NUM_BENCHMARK_RUNS = 10
+
+def deleteReadOnlyFile(action, name, exc):
+    os.chmod(name, stat.S_IWRITE)
+    os.remove(name)
+
+def prepareExecutables():
+    cwd = os.getcwd()
+    if not os.path.exists(PREV_VERSION_CLONE_DEST):
+        os.mkdir(PREV_VERSION_CLONE_DEST)
+    subprocess.run("git clone . " + PREV_VERSION_CLONE_DEST)
+    subprocess.run(f"msbuild nina-chess.sln /p:OutDir=\"{cwd}/test/\" /p:Configuration=Bench /p:Platform=x64 /p:TargetName={NEW_EXECUTABLE_NAME_WITHOUT_EXTENSION}")
+    subprocess.run(f"msbuild ./test/clone/nina-chess.sln /p:OutDir=\"{cwd}/test/\" /p:Configuration=Bench /p:Platform=x64 /p:TargetName={OLD_EXECUTABLE_NAME_WITHOUT_EXTENSION}")
+
+def cleanEnvironment():
+    if os.path.exists(PREV_VERSION_CLONE_DEST):
+        shutil.rmtree(PREV_VERSION_CLONE_DEST, onerror=deleteReadOnlyFile)
+    if os.path.exists(NEW_EXECUTABLE_PATH):
+        os.remove(NEW_EXECUTABLE_PATH)
+    if os.path.exists(OLD_EXECUTABLE_PATH):
+        os.remove(OLD_EXECUTABLE_PATH)
+
+cleanEnvironment()
+prepareExecutables()
+
+try:
+    bench_results = bench_executables.benchmarkFiles(NUM_BENCHMARK_RUNS, OLD_EXECUTABLE_PATH, NEW_EXECUTABLE_PATH)
+    if bench_results[NEW_EXECUTABLE_PATH] == EXECUTABLE_REJECTED:
+        raise Exception("Changes rejected because of significant move generation speed degradation")
+except:
+    cleanEnvironment()
+    raise
+
+
+    
