0285a92cbb53e6120adda1ea9b51c74ca8e4de75
LittleNasia <67862040+LittleNasia@users.noreply.github.com>
2024-11-11 17:07:04 +0400

    bangboo changes [NO-TEST][NO-BENCH]

--- nina-chess/bench_main.cpp
+++ nina-chess/bench_main.cpp
@@ -9,14 +9,14 @@ int main()
 	constexpr bool hidePerftOutput = true;
 	constexpr size_t benchNodeLimit = _PERFTNODES;
 
-	constexpr uint32_t numWarmupRuns = 10;
-	for (uint32_t warmupRunIndex = 0; warmupRunIndex < numWarmupRuns; warmupRunIndex++)
+	constexpr uint32_t numPerftWarmupRuns = 5;
+	for (uint32_t warmupRunIndex = 0; warmupRunIndex < numPerftWarmupRuns; warmupRunIndex++)
 	{
 		TestPerft(hidePerftOutput, benchNodeLimit);
 	}
 
 	size_t nps{ 0 };
-	constexpr uint32_t numPerftBenchRuns = 30;
+	constexpr uint32_t numPerftBenchRuns = 15;
 	for(uint32_t runIndex = 0; runIndex < numPerftBenchRuns; runIndex++)
 	{
 		size_t currRunNps = TestPerft(hidePerftOutput, benchNodeLimit);
@@ -28,14 +28,15 @@ int main()
 	std::cout << nps / numPerftBenchRuns << std::endl;
 
 	constexpr bool hideSearchTestOutput = true;
-	constexpr size_t benchDepthLimit = 5;
-	for (uint32_t warmupRunIndex = 0; warmupRunIndex < numWarmupRuns; warmupRunIndex++)
+	constexpr uint32_t numSearchWarmupRuns = 1;
+	constexpr size_t benchDepthLimit = 7;
+	for (uint32_t warmupRunIndex = 0; warmupRunIndex < numSearchWarmupRuns; warmupRunIndex++)
 	{
 		TestSearch(hideSearchTestOutput, benchDepthLimit);
 	}
 
 	nps = 0;
-	constexpr uint32_t numSearchBenchRuns = 15;
+	constexpr uint32_t numSearchBenchRuns = 2;
 	for (uint32_t runIndex = 0; runIndex < numSearchBenchRuns; runIndex++)
 	{
 		size_t currRunNps = TestSearch(hideSearchTestOutput, benchDepthLimit);
--- nina-chess/transposition_table.h
+++ nina-chess/transposition_table.h
@@ -26,13 +26,59 @@ struct TranspositionTableEntry
 class TranspositionTable : public Serializable
 {
 public:
-	TranspositionTable(const size_t sizeInMb);
+	forceinline TranspositionTable(const size_t sizeInMb);
 
-	void Insert(const TranspositionTableEntry& entry, const bool forceOverwrite);
-	const TranspositionTableEntry& Get(const uint64_t key) const;
+	forceinline void Insert(const TranspositionTableEntry& entry, const bool forceOverwrite);
+	forceinline const TranspositionTableEntry& Get(const uint64_t key) const;
 
-	void Serialize(std::ofstream& output);
-	void Deserialize(std::ifstream& input);
+	forceinline void Serialize(std::ofstream& output);
+	forceinline void Deserialize(std::ifstream& input);
 private:
 	std::vector<TranspositionTableEntry> m_Entries;
 };
+
+forceinline TranspositionTable::TranspositionTable(const size_t sizeInMb)
+{
+	const size_t sizeInBytes = sizeInMb * 1024 * 1024;
+	const size_t entryCount = sizeInBytes / sizeof(TranspositionTableEntry);
+	m_Entries.resize(entryCount);
+}
+
+forceinline void TranspositionTable::Insert(const TranspositionTableEntry& entry, const bool forceOverwrite)
+{
+	const size_t index = FastModulo(entry.Key, m_Entries.size());
+
+	const auto& currrentEntry = m_Entries[index];
+	if ((currrentEntry.Depth <= entry.Depth) || forceOverwrite)
+	{
+		m_Entries[index] = entry;
+	}
+}
+
+forceinline const TranspositionTableEntry& TranspositionTable::Get(const uint64_t key) const
+{
+	const size_t index = FastModulo(key, m_Entries.size());
+
+	return m_Entries[index];
+}
+
+forceinline void TranspositionTable::Serialize(std::ofstream& output)
+{
+	size_t entryCount = m_Entries.size();
+	output.write((char*)&entryCount, sizeof(entryCount));
+	for (const auto& entry : m_Entries)
+	{
+		output.write((char*)&entry, sizeof(entry));
+	}
+}
+
+forceinline void TranspositionTable::Deserialize(std::ifstream& input)
+{
+	size_t entryCount;
+	input.read((char*)&entryCount, sizeof(entryCount));
+	m_Entries.resize(entryCount);
+	for (auto& entry : m_Entries)
+	{
+		input.read((char*)&entry, sizeof(entry));
+	}
+}
